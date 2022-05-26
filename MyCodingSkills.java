953be08e4cdadc8ceab06a9b37e566e0af97755a
aash001 <ampinarioash@users.noreply.github.com>
2022-05-26 00:36:15 -0400

    Add solutions

---
+++ MyCodingSkills.java
@@ -0,0 +1,11 @@
+public class MyCodingSkills {
+  public static void main(String[] args) {
+    String[] programmingSkills = { "HTML", "CSS", "Git", "JavaScript", "TypeScript", "Angular", "Java", "CLI",
+        "Testing" };
+
+    for (int i = 0; i < programmingSkills.length; i++) {
+
+      System.out.println("I know how to program using " + programmingSkills[i] + ".");
+    }
+  }
+}
